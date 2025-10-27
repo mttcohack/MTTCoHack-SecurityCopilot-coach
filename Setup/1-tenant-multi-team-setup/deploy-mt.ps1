d850a7d154f4680cce48b50b36bae888182cba79
Koenraad HAadens <Koenraad@khd.be>
2025-10-27 14:39:34 +0000

    add tags

--- Setup/1-tenant-multi-team-setup/deploy-mt.ps1
+++ Setup/1-tenant-multi-team-setup/deploy-mt.ps1
@@ -6,5 +6,5 @@ if ([string]::IsNullOrEmpty($region)) {
     Write-Error "The region parameter is required. Example: ./deploy-mt -region 'northeurope' -team '1'"
     exit 1
 }
-New-AzResourceGroup -name rg-hack-${team} -location $region
+New-AzResourceGroup -Name "rg-hack-${team}" -Location $region -Tag @{SecurityControl="Ignore"; CostControl="Ignore"}
 New-AzResourceGroupDeployment -Name co-hack-deployment-${team} -ResourceGroupName rg-hack-${team}  -TemplateFile co-hack-architecture-mt.bicep  -TemplateParameterFile co-hack-architecture.parameters-mt.json -team $team -location $region
